8ff3534729a2d2e48a31af33426d223468d2ba17
VANELSLAND Marvin (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDSP) <0403412s@commun.ad.sncf.fr>
2025-07-14 10:41:42 +0200

    player-list-component

--- zombicide-frontend/src/app/app.routes.ts
+++ zombicide-frontend/src/app/app.routes.ts
@@ -1,5 +1,24 @@
 import { Routes } from '@angular/router';
+import {PagePlayerListComponent} from './pages/page-player-list/page-player-list.component';
+import {HomeComponent} from './pages/home/home.component';
+import {SelectComponent} from './pages/select/select.component';
 
 export const routes: Routes = [
-
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'select',
+    component: SelectComponent
+  },
+  {
+    path:'player-list',
+    component:PagePlayerListComponent
+  }
 ];
